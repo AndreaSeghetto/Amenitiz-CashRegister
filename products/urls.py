f37a5e90b80bc8f925a876c08ce1a1c23f0dcfba
Andrea Seghetto <andreaseghetto99@gmail.com>
2025-03-29 12:39:57 +0100

    feat: add API endpoint returning product list in JSON format

--- products/urls.py
+++ products/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.product_list, name='product_list'),
+    path('api/', views.products_api, name='products_api'),   
 ]
--- products/views.py
+++ products/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 from .models import Products
 
 def product_list(request):
     products = Products.objects.all()  # Ottieni tutti i prodotti dal database
     return render(request, 'product_list.html', {'products': products})
+
+
+def products_api(request):
+    products = Products.objects.all()
+    data = [
+        {
+            'product_code': p.product_code,
+            'name': p.name,
+            'price': float(p.price),
+        }
+        for p in products
+    ]
+    return JsonResponse(data, safe=False)
